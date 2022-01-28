03487789798284d4e29cbc38fd119b6d7fdfb86c
Omar <omarftt@gmail.com>
2022-01-28 16:51:02 -0500

    'second-commit'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>HcUy6Re2LLBRtj</h1>');
+  res.send('<div class="secret" style="display: none;">HcUy6Re2LLBRtj</div>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
